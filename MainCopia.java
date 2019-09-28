0638e76c9b1a636e5f013e5031e73b48ba5c4dbf
dgarrido7 <50625905+dgarrido7@users.noreply.github.com>
2019-09-28 10:02:53 +0200

    Creacio de Main copia

---
+++ MainCopia.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int valor=0;
+		int comp=0;
+		int max=0;
+
+		Scanner lector=new Scanner(System.in);
+		
+		for(int i=0; i<10;i++) {
+			System.out.println("Introdueix un valor nou");
+			comp=lector.nextInt();
+			if(i==0) {
+				valor=comp;
+				max=comp;
+			}
+			if(valor>=comp) {
+				valor=comp;
+			}
+			if(max<=comp){
+			max=comp;
+			}
+		}
+		
+		System.out.println(valor);
+		System.out.println(max);
+	}
+
+}
